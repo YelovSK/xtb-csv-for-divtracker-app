fd82ad6880de0c76933cef9211d706bb54990966
YelovSK <hampatrik@gmail.com>
2024-07-07 22:09:05 +0200

    args, hardcoded path, idk

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pandas as pd
 import os
+import argparse
 
-path_to_csv = input("Path to your csv file (without .csv): ")
-path_to_output_csv = input("Path for your output csv file (without .csv): ")
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True, help="Path to XTB csv file")
+ap.add_argument("-o", "--output", required=True, help="Path for your output csv file")
+args = vars(ap.parse_args())
 
-path = f"{path_to_csv}.csv"
+input_path = args["input"]
+output_path = args["output"]
 
-if os.path.exists(path):
-    data_from_xtb = pd.read_csv("../Movies/xtb.csv", usecols=["Symbol", "Comment", "Time"], sep=";")
-    data_to_app = data_from_xtb.copy()
-
-    new_columns = ["Ticker", "Quantity", "Cost Per Share", "Date"]
-    data_to_app = data_to_app.reindex(columns=new_columns)
-
-    data_to_app["Ticker"] = data_from_xtb["Symbol"].str.split(".").str[0]
-    data_to_app["Quantity"] = data_from_xtb["Comment"].str.split(" ").str[2]
-    data_to_app["Cost Per Share"] = data_from_xtb["Comment"].str.split(" ").str[4]
-    data_to_app["Date"] = pd.to_datetime(data_from_xtb["Time"].str.split(" ").str[0], dayfirst=True)
-
-    data_to_app.to_csv(f"{path_to_output_csv}.csv", index=False)
-else:
+if not os.path.exists(input_path):
     print("File does not exist")
+
+data_from_xtb = pd.read_csv(input_path, usecols=["Symbol", "Comment", "Time"], sep=";")
+data_to_app = data_from_xtb.copy()
+
+new_columns = ["Ticker", "Quantity", "Cost Per Share", "Date"]
+data_to_app = data_to_app.reindex(columns=new_columns)
+
+data_to_app["Ticker"] = data_from_xtb["Symbol"]
+data_to_app["Quantity"] = data_from_xtb["Comment"].str.split(" ").str[2].str.split("/").str[0]
+data_to_app["Cost Per Share"] = data_from_xtb["Comment"].str.split(" ").str[4]
+data_to_app["Date"] = pd.to_datetime(data_from_xtb["Time"].str.split(" ").str[0], dayfirst=True)
+
+data_to_app.to_csv(output_path, index=False, sep=";")
